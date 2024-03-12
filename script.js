67824c269916b3f35435f4fe946761dedfa3610d
Саша <mkramnichka57446@gmail.com>
2024-03-12 21:04:57 +0200

    створив

---
+++ script.js
@@ -0,0 +1,20 @@
+let slider = document.querySelector(".slider")
+let buttonLeft = document.querySelector(".arrow__left")
+let buttonRight = document.querySelector(".arrow__right")
+let sliderSlide = document.querySelectorAll(".slider__slide")
+
+let checkIndex = 0
+
+function goNextSlide() {
+    sliderSlide[checkIndex].classList.remove("active-slide")
+    checkIndex = (checkIndex + 1) % sliderSlide.length
+    sliderSlide[checkIndex].classList.add("active-slide")
+}
+buttonRight.addEventListener("click", goNextSlide)
+
+function goPrevSlide() {
+    sliderSlide[checkIndex].classList.remove("active-slide")
+    checkIndex = (checkIndex - 1 + sliderSlide.length) % sliderSlide.length
+    sliderSlide[checkIndex].classList.add("active-slide")
+}
+buttonLeft.addEventListener("click", goPrevSlide)
